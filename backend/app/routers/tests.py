3e8578de94f0653e749c6ea0884559376196ce93
MVjimbo <maslo-86@ya.ru>
2023-11-12 13:58:41 +0300

    Added boundaries in returning JSON

--- backend/app/routers/tests.py
+++ backend/app/routers/tests.py
@@ -10,9 +10,11 @@ router = APIRouter(
 
 @router.get("/TramNetwork")
 async def root(city: Annotated[str, Query(description="Название города.")]):
+    boundaries = ox.geocode_to_gdf(city)["geometry"]
     G = ox.graph_from_place(city, custom_filter='["railway"~"tram"]')
     gdf_nodes, gdf_relationships = ox.graph_to_gdfs(G)
     data = {
+        "boundaries": json.loads(boundaries.to_json()),
         "nodes": json.loads(gdf_nodes.to_json()),
         "edges": json.loads(gdf_relationships.to_json()),
     }
