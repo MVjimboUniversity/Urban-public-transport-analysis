ade2fcd618399773178bb16eebfe49deed064abf
Cr1msonEdge <dd1ns1d@gmail.com>
2023-11-16 18:39:49 +0300

    #29 Added map click support for new version

--- ui/src/components/App/Map/PolygonMap.jsx
+++ ui/src/components/App/Map/PolygonMap.jsx
@@ -1,26 +1,40 @@
-import React from "react";
-import { MapContainer, TileLayer } from 'react-leaflet'
-import { Polygon } from "react-leaflet";
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Polygon, useMapEvents } from 'react-leaflet'
 import styles from '../Map/Map.module.css'
 
 
 function PolygonMap({pos}) {
-    const blackOptions = { color: 'black' }
-    // const polygon = [
-    //     [51.515, -0.09],
-    //     [51.52, -0.1],
-    //     [51.52, -0.12],
-    //   ];
-    const Center = [(pos[0][0][0] + pos[0][1][0]) / 2, (pos[0][0][1] + pos[0][1][1]) / 2]
-    console.log("Center = ", Center);
+    const blackOptions = { color: 'black' };
+    const redOptions = {color: 'red'};
+    const Center = [(pos[0][0][0] + pos[0][1][0]) / 2, (pos[0][0][1] + pos[0][1][1]) / 2];
+    console.log("pos = ", pos);
+    // press on map
+    const [positions, setPositions] = useState([]);
+    function LocationGetter() {
+        useMapEvents({
+              click(e) {
+                  setPositions([...positions, e.latlng]);
+              }
+        });
+        return null;
+    }
+
+    function clear() {
+        setPositions([]);
+    }
     return (
-        <MapContainer className={styles.MapContainer} center={Center} zoom={13} scrollWheelZoom={true}>
-            <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Polygon positions={pos[0]} pathOptions={blackOptions}/>
-        </MapContainer>
+        <div className={styles.MapContainer}>
+            <MapContainer className={styles.Map} center={Center} zoom={13} scrollWheelZoom={true}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Polygon positions={pos[0]} pathOptions={blackOptions}/>
+                <LocationGetter/>
+                <Polygon pathOptions={redOptions} positions={positions}></Polygon>
+                <button className={styles.btn} onClick={clear}>Очистить карту</button>
+            </MapContainer>
+        </div>
     )
 }
 
--- ui/src/components/App/Map/RectangleMap.jsx
+++ ui/src/components/App/Map/RectangleMap.jsx
@@ -1,30 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from '../Map/Map.module.css'
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, Polygon, useMapEvents } from 'react-leaflet'
 import { Marker, Popup } from "react-leaflet";
 import { Rectangle } from "react-leaflet";
 
 
 function RectangleMap({pos}) {
     const blackOptions = { color: 'black' }
+    const redOptions = { color: 'red' }
     const rectangle = [
         [parseFloat(pos[0]), parseFloat(pos[2])],
         [parseFloat(pos[1]), parseFloat(pos[3])]
     ];
     const Center = [(rectangle[0][0] + rectangle[1][0]) / 2 , (rectangle[0][1] + rectangle[1][1]) / 2];
-    return (<div>
-        <MapContainer className={styles.MapContainer} center={Center} zoom={13} scrollWheelZoom={false}>
-            <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={Center}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
-            <Rectangle bounds={rectangle} pathOptions={blackOptions}/>
-        </MapContainer>
+
+    // press on map
+    const [positions, setPositions] = useState([]);
+    function LocationGetter() {
+        useMapEvents({
+              click(e) {
+                  setPositions([...positions, e.latlng]);
+              }
+        });
+        return null;
+    }
+
+    function clear() {
+        setPositions([]);
+    }
+    return (
+        <div className={styles.MapContainer}>
+            <MapContainer className={styles.Map} center={Center} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={Center}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+                <Rectangle bounds={rectangle} pathOptions={blackOptions}/>
+                <Polygon pathOptions={redOptions} positions={positions}></Polygon>
+                <LocationGetter/>
+                <button className={styles.btn} onClick={clear}>Очистить карту</button>
+            </MapContainer>
         </div>
       )
 }   
