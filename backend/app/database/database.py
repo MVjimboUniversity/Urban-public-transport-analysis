fa979ce94805d8efb05263304540a6805b877564
MVjimbo <maslo-86@ya.ru>
2023-11-29 11:58:09 +0300

    #65 : Удалил закомментированные строки

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -1,9 +1,6 @@
 import os
 import neo4j
 
-# NEO4J_URI = "neo4j://neo4j:7687"
-# NEO4J_USER = "neo4j"
-# NEO4J_PASSWORD = "123456789"
 
 NEO4J_URI = os.environ["NEO4J_URI"]
 NEO4J_USER = os.environ["NEO4J_USER"]
